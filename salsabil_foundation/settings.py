4472fa182f6e26c84edbe02be9fbc2f310d906cf
sul <sul@gmail.com>
2025-02-24 17:47:17 +0300

    debug to false

--- salsabil_foundation/settings.py
+++ salsabil_foundation/settings.py
@@ -33,7 +33,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS')
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
